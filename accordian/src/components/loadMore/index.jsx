0b701d37a72c30b9d66885481a73178c6f5b173d
MianAteeq-star <mr.unknown5264@gmail.com>
2024-06-15 11:13:41 +0500

    load more functionality done

--- accordian/src/components/loadMore/index.jsx
+++ accordian/src/components/loadMore/index.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function LoadMore() {
@@ -5,22 +6,20 @@ export default function LoadMore() {
   const [products, setProducts] = useState([]);
   const [count, setCount] = useState(0);
   const [errorMessage, setErrorMessage] = useState("");
+  const [disabled , setDisabled ]= useState(false);
 
   const HandleProducts = async () => {
     try {
       setLoading(true);
-      const response = await fetch(
-        `https://dummyjson.com/products?limit=20&skip=${
-          count === 0 ? 0 : count * 20
-        }`
-      );
-      const data = await response.json();
-      if (data && data.produts && data.products.length) {
-        setProducts(data.products);
+      let response = await axios.get(`https://dummyjson.com/products?limit=30&skip=${count === 0 ? 0 : count * 20}`)
+      console.log(response.data.products);
+      
+      if (response.data && response.data.products ) {
+        setProducts((pre)=> [...pre , ...response.data.products]);
         console.log(products);
-      }
-      setLoading(false);
-      console.log(data.products);
+        setLoading(false);
+    }
+      console.log(response.data.products);
       console.log(products);
     } catch (error) {
       console.log(error);
@@ -31,7 +30,11 @@ export default function LoadMore() {
 
   useEffect(() => {
     HandleProducts();
-  }, []);
+  }, [count]);
+
+  useEffect(() => {
+   if(products && products.length === 150) setDisabled(true)
+  }, [products]);
 
   if (loading) {
     return <div>Loading Please Wait...</div>;
@@ -42,22 +45,30 @@ export default function LoadMore() {
 
   return (
     <>
-      <div>
-        <div className="products">
-          {
-          products && products.length
+      <div  className="flex justify-center gap-4 flex-col">
+        <div className="grid grid-cols-2  md:grid-cols-3 lg:grid-cols-4  gap-3">
+          {products && products.length
             ? products.map((prod) => {
                 return (
-                  <div className="bg-slate-400 min-h-screen w-screen"  key={prod.id}>
-                   <img src={prod.thumnail} alt={prod.title} />
-                    <h1>{prod.title}</h1>
-                    {/* <h1>{prod.price}</h1> */}
-                  </div>
+                  <>
+                    
+                      <div className="flex flex-col border-2" key={prod.id}>
+                        <img src={prod.thumbnail} alt={prod.title} />
+                        <h1>{prod.title}</h1>
+                     
+                    </div>
+                  </>
                 );
               })
             : null}
         </div>
-        {/* <button>Load More</button> */}
+        <div>
+
+        <button disabled={disabled} className="my-8 p-3 bg-blue-500 rounded-xl" onClick={()=> setCount(count + 1)}>Load More</button>
+        {
+            disabled ? <p>You reached 150 products</p> :null
+        }
+        </div>
       </div>
     </>
   );
